a2f3dea4a08948d4b52b8beecc904dad4f45f819
Padraig O Conbhui <poconbhui@gmail.com>
2013-10-26 17:10:37 +0100

    Refactored code. Pulled geoscience proxying and parsing out into separate module. Pulled quickGet into separate module.

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 "use strict"
 
 var express = require('express');
-var http = require('http');
 var ejs = require('ejs');
 
 var app = express();
@@ -31,146 +30,16 @@ app.configure(function() {
 
 // Serve index
 app.get('/', function(req, res) {
-    //res.send("Go suck a brick");
     res.render('index.html');
 });
 
 
 
-//
-// Some nice http getting functions
-//
+// Serve people pages
+var people = require(__dirname + '/people');
 
-// Given an object, return a GET friendly string
-function objectToGet(obj) {
-    var ret = '';
-
-    for(var i in obj) {
-        ret += i + '=' + obj[i] + '&';
-    }
-
-    ret = ret.slice(0, -1);
-
-    return ret;
-}
-
-// Get data from a url with the given params as an object
-// and return the string of data in cb(string, err)
-function quickGet(url, params, cb) {
-    console.log(url + '?' + objectToGet(params));
-    http.get(url + '?' + objectToGet(params), function(people_res) {
-
-        var str = '';
-
-        //another chunk of data has been recieved, so append it to `str`
-        people_res.on('data', function (chunk) {
-            str += chunk;
-        });
-
-        //the whole response has been recieved, so we just print it out here
-        people_res.on('end', function () {
-            cb(str, null);
-        });
-    });
-}
-
-
-//
-// Geosciences proxying routes
-//
-
-// Proxy geosciences people page via /people
-// Send list of people as JSON
-var peopleCache = [];
-app.get('/people', function(req, res) {
-
-    // Generate request to geophysics
-    var people = 'http://www.ed.ac.uk/schools-departments/geosciences/people'
-    var params = { cw_xml: 'students.html' };
-
-    if(peopleCache.length == 0) {
-        quickGet(people, params, function(str,err) {
-            peopleCache = peoplePageToArr(str);
-
-            res.send(peopleCache);
-        });
-    }
-    else {
-        res.send(peopleCache);
-    }
-});
-
-
-// Parse the scraped people page to an array of indv ids.
-// return: [id1, id2, id3, ...]
-function peoplePageToArr(data) {
-
-    // Get links between title Full-Time... and M.Sc.
-    // Effectively, only PhD students
-    data = data.match(
-        /Full-Time Research Students.*M.Sc. Carbon Capture and Storage/
-    )[0];
-
-    // Match all person links in selected section
-    var matches = data.match(
-        /<a href=[^>]*indv=\d*&cw_xml=student.html">[^<]*<\/a>/g
-    );
-
-    // Parse the links into a format {indv, name}
-    for(var i in matches) {
-        data = matches[i].match(/indv=(\d*)&/);
-
-        matches[i] = data[1];
-    }
-
-    return matches
-}
-
-
-// Proxy geosciences individual pages via /people/:id
-var individualCache = {};
-app.get('/people/:id', function(req, res) {
-    console.log(req.params);
-
-    // Generate request to geophysics
-    var people = 'http://www.ed.ac.uk/schools-departments/geosciences/people'
-    var params = { cw_xml: 'student.html', indv: req.params.id };
-
-    if(!individualCache[req.params.id]) {
-        quickGet(people, params, function(str,err) {
-            individualCache[req.params.id] = individualPageToObj(str);
-
-            res.send(individualCache[req.params.id]);
-        });
-    }
-    else {
-        res.send(individualCache[req.params.id]);
-    }
-});
-
-
-// Parse an individual's page to an object containing a url to their
-// picture and their full name.
-// return: {face: 'url/to/face.jpg', name: 'My Name'}
-function individualPageToObj(data) {
-    var individual = {};
-
-    individual.face = (function() {
-        var matches = data.match(/src="([^"]*\/faces\/[^"]*)"/);
-
-        if(matches) return matches[1];
-        else        return 'None';
-    })();
-
-    individual.name = (function() {
-        var matches = data.match(/<div id="geosPeople"> *<h3>([^<]*)<\/h3>/);
-
-        if(matches) return matches[1];
-        else        return 'None';
-    })();
-
-    return individual;
-}
+app.get('/people', people.index);
+app.get('/people/:id', people.show);
 
 
 
---
+++ people.js
@@ -0,0 +1,142 @@
+"use strict"
+
+var quickGet = require(__dirname + '/quickGet');
+
+
+// This object provides routing for /people.
+//
+// This acts as a proxy and processor for the geosciences people pages
+// for phd students.
+//
+// The method people.index sends a JSON array containing the ids of
+// all the research students in the geoscience department
+//
+// The method people.show expects the url to be of the form /people/:id
+// and returns a JSON object containing a url for a picture of that person
+// and the person's name
+//
+var people = function() {
+
+    var people_url =
+        'http://www.ed.ac.uk/schools-departments/geosciences/people';
+
+    // Cache for processed geosciences people page
+    // Contains list of individual ids
+    var peopleCache = [];
+
+    // Cache for processed geosciences individual page
+    // Contains objects of the form
+    //   individualCache[id] = {face:'/url/to/face.jpg', name: 'My Name'}
+    var individualCache = {};
+
+
+
+    /////////////////////
+    // Routing methods //
+    /////////////////////
+
+
+    // Send JSON array containing a list of ids for all research students
+    // in geoscience
+    this.index = function(req, res) {
+
+        // Generate request to geophysics
+        var people = people_url;
+        var params = { cw_xml: 'students.html' };
+
+        if(peopleCache.length == 0) {
+            quickGet(people, params, function(str,err) {
+                peopleCache = peoplePageToArr(str);
+
+                res.send(peopleCache);
+            });
+        }
+        else {
+            res.send(peopleCache);
+        }
+    };
+
+
+
+    // Send a json object containing a url to the person's face and
+    // their name
+    this.show = function(req, res) {
+        console.log(req.params);
+
+        // Generate request to geophysics
+        var people = people_url;
+        var params = { cw_xml: 'student.html', indv: req.params.id };
+
+        if(!individualCache[req.params.id]) {
+            quickGet(people, params, function(str,err) {
+                individualCache[req.params.id] = individualPageToObj(str);
+
+                res.send(individualCache[req.params.id]);
+            });
+        }
+        else {
+            res.send(individualCache[req.params.id]);
+        }
+    };
+
+
+
+
+    ///////////////////////////////////////
+    // Scraped page processing functions //
+    ///////////////////////////////////////
+
+
+    // Parse the scraped people page to an array of indv ids.
+    // return: [id1, id2, id3, ...]
+    function peoplePageToArr(data) {
+
+        // Get links between title Full-Time... and M.Sc.
+        // Effectively, only PhD students
+        data = data.match(
+            /Full-Time Research Students.*M.Sc. Carbon Capture and Storage/
+        )[0];
+
+        // Match all person links in selected section
+        var matches = data.match(
+            /<a href=[^>]*indv=\d*&cw_xml=student.html">[^<]*<\/a>/g
+        );
+
+        // Parse the links into a format {indv, name}
+        for(var i in matches) {
+            data = matches[i].match(/indv=(\d*)&/);
+
+            matches[i] = data[1];
+        }
+
+        return matches
+    }
+
+
+    // Parse an individual's page to an object containing a url to their
+    // picture and their full name.
+    // return: {face: 'url/to/face.jpg', name: 'My Name'}
+    function individualPageToObj(data) {
+        var individual = {};
+
+        individual.face = (function() {
+            var matches = data.match(/src="([^"]*\/faces\/[^"]*)"/);
+
+            if(matches) return matches[1];
+            else        return 'None';
+        })();
+
+        individual.name = (function() {
+            var matches = data.match(/<div id="geosPeople"> *<h3>([^<]*)<\/h3>/);
+
+            if(matches) return matches[1];
+            else        return 'None';
+        })();
+
+        return individual;
+    }
+
+};
+
+
+module.exports = new people();
