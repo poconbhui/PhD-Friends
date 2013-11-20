084cdd4d286aac32645e220ccbb287d9bc602050
Padraig O Conbhui <poconbhui@gmail.com>
2013-11-20 03:59:49 +0000

    Refactored some element behaviours into singleton classes.

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,11 +8,6 @@ $(document).ready(function() {
 // Get jQuery objects for elements on the page //
 /////////////////////////////////////////////////
 
-var $face = $('#face');
-var $guessed_name = $('#guessed-name');
-var $make_guess = $('#make-guess');
-var $give_up = $('#give-up');
-var $guesses = $('#guesses');
 var $new_list = $('#new-list');
 var $list_length = $('#list-length');
 var $list_length_options = $('#list-length-options');
@@ -38,30 +33,243 @@ var name_per_round = 10;
 
 var score = new function() {
     var $score = $('#score');
-    var score = 0;
+
+    var _score = 0;
 
     this.getScore = function() {
-        return score;
+        return _score;
     }
 
     this.add = function(name) {
         var value = name.split(' ').length;
 
-        score += value;
+        _score += value;
 
-        $score.html(score);
+        $score.html(_score);
     }
 
     this.remove = function(name) {
         var value = name.split(' ').length;
 
-        score -= value;
+        _score -= value;
 
-        $score.html(score);
+        $score.html(_score);
     }
 
 
+    // Initialize score container
     $score.html('0');
+};
+
+
+var face = new function() {
+    var $face = $('#face');
+
+    var _name;
+    var _image64;
+
+
+    this.set = function(name, image64) {
+        _name = name;
+        _image64 = image64;
+
+        $face.data('name', name);
+        $face.attr('src', 'data:image/jpeg;base64,' + image64);
+    }
+
+    this.reset = function() {
+        _name = 'None';
+        _image64 = null;
+
+        $face.data('name', 'None');
+        $face.attr('src', null);
+    }
+
+
+    this.getName = function() {
+        return _name;
+    }
+    this.getImage64 = function() {
+        return _image64;
+    }
+};
+
+
+var guessed_name = new function() {
+    var $guessed_name = $('#guessed-name');
+
+
+    this.setVal = function(val) {
+        $guessed_name.val(val);
+    }
+
+    this.getVal = function() {
+        return $guessed_name.val();
+    }
+
+
+    this.setPlaceholder = function(val) {
+        if(Modernizr.input.placeholder) {
+            $guessed_name.attr('placeholder', face.getName());
+        }
+        else {
+            $guessed_name.val(face.getName());
+        }
+    }
+
+    this.clearPlaceholder = function() {
+        $guessed_name.attr('placeholder', '');
+    }
+
+
+    this.focus = function() {
+        $guessed_name.focus();
+    }
+
+
+    this.reset = function() {
+        this.setVal('');
+        this.clearPlaceholder();
+    }
+
+
+
+    // On pressing enter in the guess box, press the make guess button
+    $guessed_name.keydown(function(e) {
+        // If enter pressed
+        if(e.keyCode == 13) make_guess.check_guess();
+    });
+};
+
+
+
+var make_guess = new function() {
+    var $make_guess = $('#make-guess');
+
+
+    var _max_guesses = max_guesses; // Set from parameters at top
+    var _current_guesses;
+
+
+    this.check_guess = function() {
+        var guess = guessed_name.getVal();
+
+        // Quick check that guess is not null before anything is done
+        if(guess.length == 0) return;
+
+
+        // Reset guess field for next guess
+        guessed_name.reset();
+
+
+        // Increment current guess count
+        _current_guesses += 1;
+
+        // If over the guess limit, automatically click $give_up
+        if(_current_guesses > _max_guesses) {
+            give_up.give_up();
+        }
+
+
+        // Check correctness of guess
+        var correct = check_guess(guess);
+
+
+        if(correct) {
+            guesses.add(correct);
+
+            if(!give_up.is_given_up()) score.add(guess);
+
+            get_new_face();
+        }
+        else {
+            guesses.add({wrong:guess});
+
+            if(_current_guesses >= _max_guesses) {
+                give_up.give_up();
+                return;
+            }
+        }
+    }
+
+
+
+    this.reset = function() {
+        _current_guesses = 0;
+    }
+
+
+    //Attach check_guess to $make_guess.click
+    $make_guess.click(this.check_guess);
+};
+
+
+
+var give_up = new function() {
+    var $give_up = $('#give-up');
+
+    var _given_up = false;
+
+
+    // On giving up, put the correct answer in $guessed_name
+    // and remove the appropriate score
+    this.give_up = function() {
+        // Ensure score is only removed once
+        if(!_given_up) score.remove(face.getName());
+
+        guessed_name.setPlaceholder(face.getName());
+        guessed_name.focus();
+
+        _given_up = true;
+    }
+
+    this.is_given_up = function() {
+        return _given_up;
+    }
+
+    this.reset = function() {
+        _given_up = false;
+    }
+
+
+    this.click = function() {
+    }
+
+
+    // Attach click behaviour to element
+    $give_up.click(this.give_up);
+};
+
+
+
+var guesses = new function() {
+    var $guesses = $('#guesses');
+
+
+    this.add = function(guess) {
+        var right = guess.right;
+        var wrong = guess.wrong;
+
+        if(right && right.length) {
+            // At least one word right!
+            $guesses.append(
+                "<li class='alert-success'>"
+                    + right + " "
+                    + "<span class='text-danger'>" + wrong + "</span>"
+                + "</li>"
+            );
+        }
+        else {
+            $guesses.append(
+                "<li class='alert-danger'>" + wrong + "</li>"
+            );
+        }
+    }
+
+
+    this.reset = function() {
+        $guesses.empty();
+    }
 };
 
 
@@ -111,17 +319,15 @@ function shuffle(array) {
 
 // Reset everything
 function reset_game() {
-    $guessed_name.val('');
-    $guessed_name.attr('placeholder', '');
+    guessed_name.reset();
 
-    $face.attr('src', null);
-    $face.data('name', 'None');
+    face.reset();
 
-    $make_guess.data('num_guesses', max_guesses);
+    make_guess.reset();
 
-    $give_up.data('gave_up', false);
+    give_up.reset();
 
-    $guesses.empty();
+    guesses.reset();
 }
 
 
@@ -130,7 +336,7 @@ function reset_game() {
 function get_new_face() {
 
     // Disable answering for a minute
-    $face.data('name', 'None');
+    $('#guessed-name').prop('disabled', true);
 
     // If no students, wait a bit and try again
     if(!$new_list.data('all') || !$new_list.data('sublist')) {
@@ -156,12 +362,10 @@ function get_new_face() {
             // Clear everything
             reset_game();
 
-            $face.attr(
-                'src', 'data:image/jpeg;base64,' + individual.face
-            );
-            $face.data('name', individual.name);
+            face.set(individual.name, individual.face);
 
-            $guessed_name.focus();
+            $('#guessed-name').prop('disabled', false);
+            guessed_name.focus();
         }, 1000);
     });
 }
@@ -170,7 +374,7 @@ function get_new_face() {
 // Check that the guess in $guessed_name is either the first name or whole name
 // stored in $face.data('name')
 function check_guess(guess) {
-    var answer = $face.data('name');
+    var answer = face.getName();
     var first_name = answer.split(' ')[0];
 
     var right_pos=0;
@@ -209,70 +413,6 @@ function check_guess(guess) {
 ////////////////////////////////////////
 
 
-// On clicking "guess", check guess
-$make_guess.click(function() {
-    var guess = $guessed_name.val();
-
-    // Reset guess field for next guess
-    $guessed_name.val('');
-
-
-    // Quick check that guess is not null before num_guesses is touched
-    if(guess.length == 0) return;
-
-    var num_guesses = $make_guess.data('num_guesses') - 1;
-    $make_guess.data('num_guesses', num_guesses);
-
-    var correct = check_guess(guess);
-
-
-    if(correct) {
-        $guesses.append(
-            "<li class='alert-success'>"
-                + correct.right + " "
-                + "<span class='text-danger'>" + correct.wrong + "</span>"
-            + "</li>"
-        );
-
-        if(!$give_up.data('gave_up')) score.add(guess);
-
-        get_new_face();
-    }
-    else {
-        $guesses.append(
-            "<li class='alert-danger'>"+guess+"</li>"
-        );
-
-        if(num_guesses <= 0) {
-            $give_up.click();
-            return;
-        }
-    }
-});
-
-
-// On pressing enter in the guess box, check guess
-$guessed_name.keydown(function(e) {
-    // If enter pressed
-    if(e.keyCode == 13) $make_guess.click();
-});
-
-
-// On giving up, put the correct answer in $guessed_name
-// and remove the appropriate score
-$give_up.click(function() {
-    if(!$give_up.data('gave_up')) score.remove($face.data('name'));
-
-    if(Modernizr.input.placeholder) {
-        $guessed_name.attr('placeholder', $face.data('name'));
-        $guessed_name.focus();
-    }
-    else {
-        $guessed_name.val($face.data('name'));
-    }
-
-    $give_up.data('gave_up', true);
-});
 
 
 // Set up new list generation
